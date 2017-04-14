675b0593ec8f22455cde39af77604e39983970ba
AMER\BXK8487 <baskar_kumarasamy@homedepot.com>
2017-04-14 16:11:58 -0400

    UI Changes

--- src/main/java/com/allocations/util/MyAllocationConstants.java
+++
@@ -1,8 +0,0 @@
-package com.allocations.util;
-
-public class MyAllocationConstants {
-
-	public static final String ALLOCATION_QUERY = "SELECT EMP_ID, EMP_NAME, PORTFOLIO, PROJECT,"
-			+ " TYPE_LD_SR, RATE_PER_HR, BILLING_HRS, BILLING, LEAVE_HRS,"
-			+ " LEAKAGE_HRS, COMMENTS FROM MYALLOCATION.ALLOCATIONS WHERE EMP_ID = :empId ";
-}
